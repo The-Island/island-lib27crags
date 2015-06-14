d50987d5b595475cea38703c6dbb00cdf8d737fc
Eyal Cohen <eyalcohen1@gmail.com>
2015-06-14 00:52:02 -0400

    use http to speed things up

--- tests/test.js
+++ tests/test.js
@@ -4,16 +4,16 @@ var lib27crags = require('../lib/27crags');
 
 console.log('Searching for Dan Michels');
 
-/*
-lib27crags.searchUser('Sean', function(err, res) {
+lib27crags.searchUser('dan michels', function(err, res) {
   console.log(err, res);
 });
 
 console.log('Getting ticks for Dan Michels');
-*/
 
+/*
 lib27crags.getTicks('danmichels', function(err, res) {
   //console.log(err, res.length);
   console.log(err, res);
 });
+*/
 

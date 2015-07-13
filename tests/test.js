620b6547c6d490960d3d44533caf6f9dd7f6aac1
Eyal Cohen <eyalcohen1@gmail.com>
2015-07-12 23:38:32 -0700

    Send the right cookies for 27crags

--- tests/test.js
+++ tests/test.js
@@ -2,9 +2,11 @@
 
 var lib27crags = require('../lib/27crags');
 
+/*
 lib27crags.searchUser('bill', function(err, res) {
   console.log(err, res);
 });
+*/
 
 lib27crags.getTicks('wmmurray', function(err, res) {
   //console.log(err, res.length);
